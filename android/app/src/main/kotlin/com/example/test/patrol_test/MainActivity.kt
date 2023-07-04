7f9df6ecbf36ccaa2cb37bdf644a2d8cd3cab6ea
Agata Orlowska <agata.olbrycht@gmail.com>
2023-07-04 09:59:15 +0200

    1456 reproduce bug

---
+++ android/app/src/main/kotlin/com/example/test/patrol_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test.patrol_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_driver/app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:patrol_test/main.dart' as app;
+
+void main() async {
+  enableFlutterDriverExtension();
+   app.main();
+}
